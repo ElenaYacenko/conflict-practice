6607e73506fa2619bbe816a683585606a35b282e
ElenaYacenko <149763977+ElenaYacenko@users.noreply.github.com>
2026-05-25 16:30:56 +0300

    feat: add build.gradle with dependencies

---
+++ src/test/java/NegativeTests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import com.codeborne.selenide.Condition;
+
+public class NegativeTests {
+
+    @Test
+    void loginPageShouldHaveEmptyFields() {
+        open("https://lknew.ch-sk.ru/person-auth-form");
+
+        // Проверяем, что поле логина существует и пустое
+        $("#iPhoneOrEmail").shouldBe(Condition.visible);
+        String loginValue = $("#iPhoneOrEmail").getValue();
+
+        // Проверяем, что поле пароля существует и пустое
+        $("#iPassword").shouldBe(Condition.visible);
+        String passwordValue = $("#iPassword").getValue();
+
+        // Проверяем кнопку "Войти" по id
+        $("#cSignIn").shouldBe(Condition.visible).shouldHave(Condition.text("Войти"));
+    }
+
+}
